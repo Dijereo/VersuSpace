a778fbbf1f48b7c5215a621ca253c16db547e4b2
Dion Recai <dijereo909@gmail.com>
2020-08-29 17:10:00 -0400

    Added button

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import pygame
 
 
+class Button:
+    def draw(self, window):
+        font = pygame.font.SysFont('Consolas', 30)
+        text_surface = font.render('Some Text', False, (0, 0, 0))
+        window.blit(text_surface, (0, 0))
+
+
 class Screen:
     def draw(self, window):
         raise NotImplementedError('Screen class draw method is abstract')
@@ -8,7 +15,9 @@ class Screen:
 
 class MenuScreen(Screen):
     def draw(self, window):
+        button = Button()
         window.fill((0, 0, 100))
+        button.draw(window)
 
 
 class GameScreen(Screen):
@@ -17,27 +26,26 @@ class GameScreen(Screen):
 
 
 class EventQueue:
-    def sendEventsTo(self, handler):
+    def send_events_to(self, handler):
         for event in pygame.event.get():
             handler.handle(event)
 
 
 class EventHandler:
-    def __init__(self, app, listeners):
-        self.app = app
+    def __init__(self, listeners):
         self.listeners = listeners
         
     def handle(self, event):
         for listener in self.listeners:
-            if listener.hasFound(event):
-                listener.performAction()
+            if listener.has_found(event):
+                listener.perform_action()
 
 
 class EventListener:
-    def hasFound(self, event):
+    def has_found(self, event):
         raise NotImplementedError('EventListener class hasFound method is abstract')
 
-    def performAction(self):
+    def perform_action(self):
         raise NotImplementedError('EventListener class performAction method is abstract')
 
 
@@ -45,10 +53,10 @@ class AppQuitListener(EventListener):
     def __init__(self, app):
         self.app = app
 
-    def hasFound(self, event):
+    def has_found(self, event):
         return event.type == pygame.QUIT
 
-    def performAction(self):
+    def perform_action(self):
         self.app.stop()
 
 
@@ -56,50 +64,42 @@ class StartGameListener(EventListener):
     def __init__(self, app):
         self.app = app
         
-    def hasFound(self, event):
+    def has_found(self, event):
         return event.type == pygame.MOUSEBUTTONDOWN
 
-    def performAction(self):
-        self.app.startGame()
+    def perform_action(self):
+        self.app.start_game()
 
 
 class App:
     def __init__(self):
-        self.setupWindow()
-        self.setupScreens()
-        self.setupEvents()
+        self.window = pygame.display.set_mode((500, 500))
+        pygame.display.set_caption('VersuSpace')
+        self.menu_screen = MenuScreen()
+        self.game_screen = GameScreen()
+        self.current_screen = self.menu_screen
+        self.quit_listener = AppQuitListener(self)
+        self.start_game_listener = StartGameListener(self)
+        self.queue = EventQueue()
+        self.handler = EventHandler([self.quit_listener, self.start_game_listener])
         self.running = True
 
     def run(self):
         while self.running:
-            self.queue.sendEventsTo(self.handler)
-            self.currentscreen.draw(self.window)
+            self.queue.send_events_to(self.handler)
+            self.current_screen.draw(self.window)
             pygame.display.update()
 
     def stop(self):
         self.running = False
 
-    def startGame(self):
-        self.currentscreen = self.gamescreen
-
-    def setupWindow(self):
-        self.window = pygame.display.set_mode((500, 500))
-        pygame.display.set_caption('VersuSpace')
-
-    def setupScreens(self):
-        self.menuscreen = MenuScreen()
-        self.gamescreen = GameScreen()
-        self.currentscreen = self.menuscreen
-
-    def setupEvents(self):
-        self.quitlistener = AppQuitListener(self)
-        self.startgamelistener = StartGameListener(self)
-        self.queue = EventQueue()
-        self.handler = EventHandler(self, [self.quitlistener, self.startgamelistener])
+    def start_game(self):
+        self.current_screen = self.game_screen
 
 
 def main():
     pygame.init()
+    pygame.font.init()
     app = App()
     app.run()
     pygame.quit()
