b70b7085aa4fd5e545952c5d51969c668a07b42f
Dion Recai <dijereo909@gmail.com>
2020-08-30 18:12:35 -0400

    Moved rocket center of rotation from apex to geometric center

--- rocket.py
+++ rocket.py
@@ -5,9 +5,10 @@ from event import *
 
 class Rocket:
     def __init__(self, pos, angle, color):
-        self.pos = Vector2(pos)
-        self.velocity = Vector2(1, 1)
-        self.acceleration = Vector2(-0.001, -0.002)
+        self.position = Vector2(pos)
+        self.velocity = Vector2(0, 0)
+        self.acceleration = Vector2(-0.005, -0.004)
+        self.angular = 0
         self.angle = angle
         self.color = color
         self.thrust = False
@@ -16,31 +17,34 @@ class Rocket:
         pygame.draw.polygon(window, self.color, self.get_polygon())
 
     def move(self, dt, screen_size):
-        self.pos += dt * self.velocity
+        self.position += dt * self.velocity
         self.velocity += dt * self.acceleration
         self.try_wrap(screen_size[0], screen_size[1])
 
     def get_polygon(self):
         vertex1 = Vector2()
-        vertex1.from_polar((25, self.angle - 15))
-        vertex1 = self.pos - vertex1
+        vertex1.from_polar((16, self.angle))
+        vertex1 = vertex1 + self.position
         vertex2 = Vector2()
-        vertex2.from_polar((25, self.angle + 15))
-        vertex2 = self.pos - vertex2
-        return [self.pos, vertex1, vertex2]
+        vertex2.from_polar((12, self.angle + 225))
+        vertex2 = self.position + vertex2
+        vertex3 = Vector2()
+        vertex3.from_polar((12, self.angle - 225))
+        vertex3 = self.position + vertex3
+        return [vertex1, vertex2, vertex3]
 
     def set_thrust(self, on):
         self.thrust = on
 
     def try_wrap(self, width, height):
-        if self.pos.x < 0:
-            self.pos.x = width
-        elif self.pos.x > width:
-            self.pos.x = 0
-        if self.pos.y < 0:
-            self.pos.y = height
-        elif self.pos.y > height:
-            self.pos.y = 0
+        if self.position.x < 0:
+            self.position.x = width
+        elif self.position.x > width:
+            self.position.x = 0
+        if self.position.y < 0:
+            self.position.y = height
+        elif self.position.y > height:
+            self.position.y = 0
 
 
 class ThrustListener(KeyPressedListener):
