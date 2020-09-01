837a6c4c790ed2ace51600348f565c6d1aefbcd7
Dion Recai <dijereo909@gmail.com>
2020-09-01 16:07:11 -0400

    Added list of rockets

--- game_screen.py
+++ game_screen.py
@@ -5,20 +5,21 @@ from rocket import *
 class GameScreen(Screen):
     def __init__(self, app):
         super().__init__(app)
-        self.rocket = Rocket((app.width // 4, app.height // 4), 270, Colors.RED)
-        self.rocket2 = Rocket((3 * app.width // 4, 3 * app.height // 4), 90, Colors.BLUE)
-        self.handler.add_state_listeners([
-            ThrustListener(self.rocket, pygame.K_UP),
-            RotationListener(self.rocket, pygame.K_RIGHT, pygame.K_LEFT),
-            ThrustListener(self.rocket2, pygame.K_w),
-            RotationListener(self.rocket2, pygame.K_a, pygame.K_d)
-        ])
+        rocket_data = [
+            (app.width // 4, app.height // 4, 270, Colors.RED, pygame.K_UP, pygame.K_RIGHT, pygame.K_LEFT),
+            (3 * app.width // 4, 3 * app.height // 4, 90, Colors.BLUE, pygame.K_w, pygame.K_a, pygame.K_d)]
+        self.rockets = []
+        for posx, posy, angle, color, thrust_key, cw_key, ccw_key in rocket_data:
+            self.rockets.append(Rocket((posx, posx), angle, color))
+            self.handler.add_state_listeners([
+                ThrustListener(self.rockets[-1], thrust_key),
+                RotationListener(self.rockets[-1], cw_key, ccw_key)])
 
     def step(self, time):
-        self.rocket.move(time, (self.width, self.height))
-        self.rocket2.move(time, (self.width, self.height))
+        for rocket in self.rockets:
+            rocket.move(time, (self.width, self.height))
 
     def draw(self, window):
         window.fill(Colors.BLACK)
-        self.rocket.draw(window)
-        self.rocket2.draw(window)
+        for rocket in self.rockets:
+            rocket.draw(window)
