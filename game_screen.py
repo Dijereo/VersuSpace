e0e5084191af75d722149c29b765adb949f5b1f0
Dion Recai <dijereo909@gmail.com>
2020-09-01 15:57:02 -0400

    Added second rocket movement

--- game_screen.py
+++ game_screen.py
@@ -9,10 +9,14 @@ class GameScreen(Screen):
         self.rocket2 = Rocket((3 * app.width // 4, 3 * app.height // 4), 90, Colors.BLUE)
         self.handler.add_state_listeners([
             ThrustListener(self.rocket, pygame.K_UP),
-            RotationListener(self.rocket, pygame.K_RIGHT, pygame.K_LEFT)])
+            RotationListener(self.rocket, pygame.K_RIGHT, pygame.K_LEFT),
+            ThrustListener(self.rocket2, pygame.K_w),
+            RotationListener(self.rocket2, pygame.K_a, pygame.K_d)
+        ])
 
     def step(self, time):
         self.rocket.move(time, (self.width, self.height))
+        self.rocket2.move(time, (self.width, self.height))
 
     def draw(self, window):
         window.fill(Colors.BLACK)
